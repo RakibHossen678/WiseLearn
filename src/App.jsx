ba8f29a26528adcabf533f50d1638b9b4905fb65
RakibHossen678 <rh491464@gmail.com>
2024-10-06 16:41:50 +0600

    initial setup created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button variant="destructive">Clfick</Button>
+    </>
+  );
+}
+
+export default App;
